d7831946a2487ace6f186e9d24614c06869b479a
[Prabhav Mehra s3743761] <s3743761@student.rmit.edu.au>
2021-01-14 21:08:03 +1100

    Connecting to api gateway and lambda

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 import boto3
 from botocore.exceptions import ClientError
+import requests
 
 app = Flask(__name__)
 APP_CLIENT_ID = "281hf825n7bh0t0s55giarg103"
@@ -106,6 +107,41 @@ def forgot_password():
         print(e)
     return redirect(url_for('lobby'))
 
+@app.route('/auth/login', methods=['GET','POST'])
+def login():
+    user_email = request.form['Email']
+    password = request.form['Password']
+
+    idp_client = boto3.client('cognito-idp')
+    response = None
+    try:
+        response =  idp_client.initiate_auth(ClientId=APP_CLIENT_ID,
+                                    AuthFlow='USER_PASSWORD_AUTH',
+                                    AuthParameters={
+                                    'USERNAME': user_email,
+                                    'PASSWORD': password
+                                    }
+        )
+        # idp_client.login(ClientId=APP_CLIENT_ID,
+        #                            Username=user_email,
+        #                            password =password)
+        
+                
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'UserNotFoundException':
+            # Todo Handle Not Found User
+            print("Can't Find user by Email")
+        if e.response['Error']['Code'] == 'ParamValidationError':
+            # Todo Handle Param Validate
+            print("Param Validate Error")
+        print(e)
+    
+    r = requests.get("https://8c7ymla190.execute-api.us-west-2.amazonaws.com/dev/test_auth", 
+    headers={"Authorization": response['AuthenticationResult']['IdToken']})
+
+    print(r)
+
+    return render_template('home.html')
 
 @app.route('/auth/confirm/forgot/password/', methods=['POST'])
 def confirm_forgot_password():
