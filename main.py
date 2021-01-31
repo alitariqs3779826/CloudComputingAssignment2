01260c6b0a941e9f7ba4b8408c5f41926ba46bf0
Muhammad Tariq <62224301+alitariqs3779826@users.noreply.github.com>
2021-01-31 20:50:57 +1100

    deployed on ec2

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
+
+import os
+from flask import Flask, render_template, redirect, url_for, request, jsonify, session
 import boto3
-import json
+from botocore.exceptions import ClientError
+import requests
+import os
+import botocore
+from booking import dynamoRoute
+from cognito import cognitoRoute
+from s3 import s3Route
 
-with open('happy.jpg', 'rb') as image_data:
-    response_content = image_data.read()
+app = Flask(__name__)
+APP_CLIENT_ID = "281hf825n7bh0t0s55giarg103"
+app.config["IMAGE_UPLOADS"] = "static/img"
+app.secret_key = '281hf825n7bh0t0s55giarg103'
 
-def detect_faces(photo):
+app.register_blueprint(s3Route)
+app.register_blueprint(cognitoRoute)
+app.register_blueprint(dynamoRoute)
 
-    client=boto3.client('rekognition',aws_access_key_id='ASIAXCDX35MMYLRMLFJC',
-             aws_secret_access_key='wZg/J0K1J3zDfVYaNfF7g989xKqBBT313SpBasua',
-             aws_session_token='FwoGZXIvYXdzEB4aDG09R52nL11YckLRkSLMAX8mJPY4gyZNkZpFcRpm/XSWZgfpfyeLUVKJLQxf2AE3APIAEhzjdFa8bLiIMSkBWKAj6kFcXAQ6mOJQovD/CIsG6BXYEDr0e4XJvhWlRxsDKb928VF5gpCXly5BT6JxaVveKC377jCnZTYUA+TYJ2BVO+niUDi3aVFLpWBEhYUulFmWdiNviA6Rr4A/0zRQdW1JYlKlNTBSE6mmMgvxtKB4qdVs1r+4l573vrXF8t871XB4I02yhvUrxAkorjhX+zaB8nf1PLluYKA9VCjjxfv/BTIttsxPs8837c8EuEuJBQK89AJSqlzKGK34KVurU2rEHRrVQ3f3AZYO+YnZ/GDJ',
-             region_name='us-east-1')
-
-    response = client.detect_faces(Image={'Bytes':response_content}, Attributes=['ALL'])
-
-    print('Detected faces for ' + photo)    
-    for faceDetail in response['FaceDetails']:
-        print('The detected face is between ' + str(faceDetail['AgeRange']['Low']) 
-              + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
-        print('Here are the other attributes:')
-        print(json.dumps(faceDetail, indent=4, sort_keys=True))
-
-    return len(response['FaceDetails'])
-
-def main():
-    photo='happy.jpg'
-    face_count=detect_faces(photo)
-    print("Faces detected: " + str(face_count))
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+   
+    app.run(debug=True)
